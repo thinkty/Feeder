277a42c7914d93143d20b379c56820d61c20f8a8
thinkty <thinkty@naver.com>
2020-07-27 21:32:16 +0900

    Showing Dialog on title click #2

--- src/components/FeedCard/FeedCardTitle.js
+++ src/components/FeedCard/FeedCardTitle.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import { Typography } from '@material-ui/core';
+import {
+  Typography,
+  Dialog,
+  IconButton,
+  Grid
+} from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
 
 const palette = require('../../configs/palette.json');
 
@@ -12,24 +18,77 @@ export default class FeedCardTitle extends Component {
     super(props);
     this.state = {
       width: this.props.width,
-      title: this.props.title
+      title: this.props.title,
+      open: false
     };
+  }
+
+  openDialog = () => {
+    this.setState({
+      open: true
+    });
+  }
+
+  closeDialog = () => {
+    this.setState({
+      open: false
+    });
   }
 
   render() {
     return (
-      <Typography
-        variant="body1"
-        noWrap
-        gutterBottom
-        style={{
-          color: palette.text.feedcard.title,
-          fontWeight: "normal",
-          width: this.state.width * 0.8
-        }}
-      >
-        { this.state.title }
-      </Typography>
+      <div>
+        <div 
+          onClick={this.openDialog}
+        >
+          <Typography
+            variant="body1"
+            noWrap
+            gutterBottom
+            style={{
+              color: palette.text.feedcard.title,
+              fontWeight: "normal",
+              width: this.state.width * 0.8
+            }}
+          >
+            { this.state.title }
+          </Typography>
+        </div>
+        <Dialog
+          open={this.state.open}
+          onClose={this.closeDialog}
+        >
+          <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+          >
+            <Grid item>
+              <Typography
+                variant="body1"
+                style={{
+                  color: palette.text.feedcard.title,
+                  margin: '10px'
+                }}
+              >
+                { this.state.title }
+              </Typography>
+            </Grid>
+            <Grid item>
+              <IconButton
+                onClick={this.closeDialog}
+                style={{
+                  color: '#8b96a4',
+                  margin: '10px'
+                }}
+              >
+                <CloseIcon />
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Dialog>
+      </div>
     );
   }
 }
