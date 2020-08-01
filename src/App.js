485fc964885ca815ae891ce6d12f5f7d4a2a5523
thinkty <thinkty@naver.com>
2020-08-01 16:42:53 +0900

    Setting feedlist on initial render

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import {
 } from '@material-ui/core';
 import 'typeface-muli';
 import MainFeedPage from './components/MainFeedPage';
-import { getItem, setItem } from './utils/localstorageHandler';
+import { getItem, setItem, checkItem } from './utils/localstorageHandler';
 
 function App() {
 
   // If color does not exist, read from palette.json and save the result to 
   // local storage
   let colors;
-  if (getItem('colors', true)) {
+  if (checkItem('colors')) {
     colors = getItem('colors', true);
   } else {
     colors = require('./configs/palette.json');
@@ -22,7 +22,9 @@ function App() {
 
   // If feedlist does not exist, read from feedlist.json and save the result to
   // local storage
-  
+  if (!checkItem('feedlist')) {
+    setItem('feedlist', require('./configs/feedlist.json'), true);
+  }
 
   return (
     <ThemeProvider 
